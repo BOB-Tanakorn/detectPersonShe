4a895e10d634f750ff6041de9dada919d498e0eb
tanakorn <tanakorn.ptf01@gmail.com>
2022-09-06 09:59:21 +0700

    deploy

--- detectPersonShe.py
+++ detectPersonShe.py
@@ -17,60 +17,91 @@ cursorUpdate = cursorUpdate.cursor()
 countGroundMixer = 0
 countGroundPL = 0
 
-startGroundMixer = 12
-startGroundPL = 12
+startGroundMixer = 120
+startGroundPL = 120
 
 while True:
-    #connect database
-    connectRunPrograms = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
-    cursorPrograms = connectRunPrograms.cursor()
-    sqlCmdSelect = 'SELECT status FROM statusPrograms'
-    cursorPrograms.execute(sqlCmdSelect)
-    cursorPrograms = cursorPrograms.fetchall()
-    for i in cursorPrograms:
-        if i[0] == True:
-            status = False
-            break
-        else:
-            status = True
-
     countGroundMixer += 1
     countGroundPL += 1
-    if status == True:
-        
-        if countGroundMixer >= startGroundMixer:
-            print('{} >>> start process location groundFloorMixer'.title().format(datetime.datetime.now()))
-            os.system(pathBacthFile + '/groundFloorMixer.bat')
-            time.sleep(3)
-            ltGroundFloorMixer = 'groundFloorMixer'
-            ltGroundFloorMixer = ltGroundFloorMixer.replace("'", "")
-            statusPersonGroundMixer = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
-            statusPersonGroundMixer = statusPersonGroundMixer.cursor()
-            statusPersonGroundMixer = statusPersonGroundMixer.execute('SELECT statusPerson FROM detectPersonShe WHERE location=?', ltGroundFloorMixer)
-            statusPersonGroundMixer = statusPersonGroundMixer.fetchone()[0]
-            if statusPersonGroundMixer == True:
-                startGroundMixer = 60
-                countGroundMixer = 0
-            else:
-                countGroundMixer = 0
-            print('{} >>> end process location groundFloorMixer'.title().format(datetime.datetime.now()))
-        
-        if countGroundPL >= startGroundPL:
-            print('{} >>> start process location groundFloorPL'.title().format(datetime.datetime.now()))
-            os.system(pathBacthFile + '/groundFloorPL.bat')
-            time.sleep(3)
-            ltGroundFloorPL = 'groundFloorPL'
-            ltGroundFloorPL = ltGroundFloorPL.replace("'", "")
-            statusPersonGroundPL = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
-            statusPersonGroundPL = statusPersonGroundPL.cursor()
-            statusPersonGroundPL = statusPersonGroundPL.execute('SELECT statusPerson FROM detectPersonShe WHERE location=?', ltGroundFloorPL)
-            statusPersonGroundPL = statusPersonGroundPL.fetchone()[0]
-            if statusPersonGroundPL == True:
-                startGroundPL = 60
-                countGroundPL = 0
-            else:
-                countGroundPL = 0
-            print('{} >>> end process location groundFloorPL'.title().format(datetime.datetime.now()))
 
-    time.sleep(10)
+    if countGroundMixer >= startGroundMixer:
+        runGroundMixer = True
+        while runGroundMixer:
+            #connect database
+            connectRunPrograms = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
+            cursorPrograms = connectRunPrograms.cursor()
+            sqlCmdSelect = 'SELECT status FROM statusPrograms'
+            cursorPrograms.execute(sqlCmdSelect)
+            cursorPrograms = cursorPrograms.fetchall()
+            for iGroundMixer in cursorPrograms:
+                if iGroundMixer[0] == True:
+                    statusGrounsMixer = False
+                    break
+                else:
+                    statusGrounsMixer = True
+
+            time.sleep(5)
+
+            if statusGrounsMixer == True:
+                print('{} >>> start process location groundFloorMixer'.title().format(datetime.datetime.now()))
+                os.system(pathBacthFile + '/groundFloorMixer.bat')
+                time.sleep(3)
+                ltGroundFloorMixer = 'groundFloorMixer'
+                ltGroundFloorMixer = ltGroundFloorMixer.replace("'", "")
+                statusPersonGroundMixer = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
+                statusPersonGroundMixer = statusPersonGroundMixer.cursor()
+                statusPersonGroundMixer = statusPersonGroundMixer.execute('SELECT statusPerson FROM detectPersonShe WHERE location=?', ltGroundFloorMixer)
+                statusPersonGroundMixer = statusPersonGroundMixer.fetchone()[0]
+                if statusPersonGroundMixer == True:
+                    startGroundMixer = 600
+                    countGroundMixer = 0
+                    timeDelay = 1
+                else:
+                    startGroundMixer = 120
+                    countGroundMixer = 0
+
+                print('{} >>> end process location groundFloorMixer'.title().format(datetime.datetime.now()))
+                runGroundMixer = False
+
+             
+    if countGroundPL >= startGroundPL:
+        runGroundPL = True
+        while runGroundPL:
+            #connect database
+            connectGroundPL = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
+            cursorGroundPL = connectGroundPL.cursor()
+            cmdGroundPL = 'SELECT status FROM statusPrograms'
+            cursorGroundPL.execute(cmdGroundPL)
+            cursorGroundPL = cursorGroundPL.fetchall()
+            for iGroundPL in cursorGroundPL:
+                if iGroundPL[0] == True:
+                    statusGrounsPL = False
+                    break
+                else:
+                    statusGrounsPL = True
+
+                print('iGroundPL[0] =', iGroundPL[0])
+            time.sleep(5)
+
+            if statusGrounsPL == True:
+                print('{} >>> start process location groundFloorPL'.title().format(datetime.datetime.now()))
+                os.system(pathBacthFile + '/groundFloorPL.bat')
+                time.sleep(3)
+                ltGroundFloorPL = 'groundFloorPL'
+                ltGroundFloorPL = ltGroundFloorPL.replace("'", "")
+                statusPersonGroundPL = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server}; SERVER=localhost; DATABASE=projectComputerVision; UID=sa; PWD=123456")
+                statusPersonGroundPL = statusPersonGroundPL.cursor()
+                statusPersonGroundPL = statusPersonGroundPL.execute('SELECT statusPerson FROM detectPersonShe WHERE location=?', ltGroundFloorPL)
+                statusPersonGroundPL = statusPersonGroundPL.fetchone()[0]
+                if statusPersonGroundPL == True:
+                    startGroundPL = 600
+                    countGroundPL = 0
+                else:
+                    startGroundPL = 120
+                    countGroundPL = 0
+                    
+                print('{} >>> end process location groundFloorPL'.title().format(datetime.datetime.now()))
+                runGroundPL = False
+
+    time.sleep(1)
     # time.sleep(5)
